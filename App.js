cbfb078e7a62e368a6f941541ec1ceafd631d94e
Animesh-weavers <animesh.dey@weavers-web.com>
2022-05-21 01:48:36 +0530

    create component  and view image

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 
-import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView, Text, TextInput, View, Image} from 'react-native';
+
+const Othercom = props => {
+  return (
+    <View>
+      <Text>Other Component in {props.tech}</Text>
+    </View>
+  );
+};
+
+const CatApp = () => {
+  return (
+    <View>
+      <Image
+        source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png'}}
+        style={{width: 200, height: 200}}
+      />
+      <Text>Hello, I am your cat!</Text>
+    </View>
+  );
+};
 
 const App = () => {
   const getFullName = (fname, lname) => {
@@ -17,6 +29,27 @@ const App = () => {
   return (
     <SafeAreaView>
       <Text>React-Native {getFullName('Animesh', 'Dey')}</Text>
+      <View style={{padding: 15}}>
+        <TextInput
+          style={{
+            height: 40,
+            borderColor: 'gray',
+            borderWidth: 1,
+          }}
+          defaultValue="Name"
+        />
+      </View>
+      <View style={{margin: 15}}>
+        <Othercom tech="React Native" />
+        <Othercom tech="React Native" />
+        <Othercom tech="React Native" />
+        <Othercom tech="React Native" />
+        <Othercom tech="React Native" />
+        <Othercom tech="React Native" />
+      </View>
+      <View style={{padding:25}}>
+        <CatApp />
+      </View>
     </SafeAreaView>
   );
 };
